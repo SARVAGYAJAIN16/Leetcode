2615c91cde96e817d8e47675706eed95836b9df8
SARVAGYAJAIN16 <90783021+SARVAGYAJAIN16@users.noreply.github.com>
2024-05-22 17:30:17 +0530

    Time: 15 ms (20.01%), Space: 8.2 MB (78.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int last;
         long revnum=0;
-        int dup=x;
-        while(x>0)
-        {
-            last=x%10;
-            revnum=(revnum*10)+last;
+        int rev = x;
+         
+         while(x>0)
+         {
+            long last = x % 10;
+            revnum = revnum*10 + last;
             x=x/10;
-        }
-        if(revnum == dup) return true;
-        else return false;
+         }
+         if(revnum==rev)
+         {
+            return true;
+         }
+         else{
+            return false;
+         }
     }
 };
