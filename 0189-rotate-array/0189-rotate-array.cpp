25b69e491e5c56f4ceafc0d1e34aebf12c9e1ca5
SARVAGYAJAIN16 <90783021+SARVAGYAJAIN16@users.noreply.github.com>
2024-05-29 23:06:48 +0530

    Update 0189-rotate-array.cpp

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,14 +1,18 @@
+// here done by right rotate and another platform left rotate.......................
 #include<bits/stdc++.h>
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         k = k % nums.size();
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-  //      reverse(nums.begin() , nums.begin() + k);
- //       reverse( nums.begin() + k , nums.end());
- //       reverse(nums.begin(),nums.end());
+                       reverse(nums.begin(),nums.end());      // ****************Right rotate*****************************
+                       reverse(nums.begin(),nums.begin()+k);
+                       reverse(nums.begin()+k,nums.end());
+
+        
+                  //****************************** left rotate *****************************************************
+                   //      reverse(nums.begin() , nums.begin() + k);
+                  //       reverse( nums.begin() + k , nums.end());
+                 //       reverse(nums.begin(),nums.end());
     } 
 };
 
